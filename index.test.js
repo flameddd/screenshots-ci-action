ccc2c9a782d24feb8ca6b86e0873efb9be5e5b96
Jacky Hu <flameddd@gmail.com>
2020-02-17 03:37:37 +0800

    update test yaml, output error msg

--- index.test.js
+++ index.test.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 test('test runs', () => {
   const ip = path.join(__dirname, 'index.js');
-  console.log(__dirname)
-  console.log(ip)
-  console.log(cp.execSync(`node ${ip}`).toString());
+  cp.exec(`node ${ip}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
 })
